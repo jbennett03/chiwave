fa76be9b282b64442880ce577d09379523951c92
Jalen Bennett <jalenjbennett@gmail.com>
2026-09-02 22:00:56 -0500

    edited some code due to errors

--- components/albums/AlbumGrid.tsx
+++ components/albums/AlbumGrid.tsx
@@ -15,7 +15,7 @@ export function AlbumGrid({ albums }: { albums: Album[] }) {
   return (
     <div className="max-w-content mx-auto px-8 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-7 py-12 pb-20">
       {albums.map((album, i) => (
-        <AlbumCard key={album.spotifyId} album={album} index={i} />
+        <AlbumCard key={album.mbid} album={album} index={i} />
       ))}
     </div>
   )
